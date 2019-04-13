9b122938c5de6733c18ab6341029915a0ef6c47c
Paul Friel <paul.b.friel@me.com>
2019-04-13 18:51:17 +0000

    Added createAnalysis function for crossfilter to pass to the function to create graphical design

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,7 +1,21 @@
 queue()
     .defer(d3.csv, "data/Studentsperformance.csv")
-    .await(makeGraphs);
+    .await(createAnalysis);
+
+function createAnalysis(error, studentData) {
+    var ndx = crossfilter(studentData); //Crossfilter gets passed to the function that will draw the graph
     
-function makeGraphs(error, studentData) {
-    
-}    
+    show_student_selector(ndx);
+
+    dc.renderAll(); // Essential command for Chart to appear
+}
+
+function show_student_selector(ndx) {
+    var studentDim = ndx.dimension(dc.pluck('ethnicity')); // Both dimension & group created and passed back to dimensional charting select menu
+    var studentGroup = studentDim.group();
+
+    dc.selectMenu('#student-selector') // x2 properties only
+        .dimension(studentDim)
+        .group(studentGroup);
+        
+}
