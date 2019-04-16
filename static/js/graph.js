0a2b7b765b24e57f2f639ccdf4bb78fefa340611
Paul Friel <paul.b.friel@me.com>
2019-04-16 20:56:16 +0000

    Use of ParseInt to convert string data to numbers and ordered data to a numerical order

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,15 +1,21 @@
 queue()
-    .defer(d3.csv, "data/Studentsperformance.csv")
+    .defer(d3.csv, "data/studentsperformanceData.csv")
     .await(createAnalysis);
 
-function createAnalysis(error, studentData) {
-    var ndx = crossfilter(studentData); //Crossfilter gets passed to the function that will draw the graph
-       
+function createAnalysis(error, studentsperformanceData) {
+    var ndx = crossfilter(studentsperformanceData); //Crossfilter gets passed to the function that will draw the graph
+            studentsperformanceData.forEach(function(d){ 
+                d.student = parseInt(d["student"]);  // Converts string to numbers
+                d.math_score = parseInt(d["math.score"]);  // Converts string data to Integers.     
+            }); 
+     
     show_student_selector(ndx);
     show_gender_selector(ndx);
     
     show_gender_percentage(ndx, "female", "#female-student-percentage"); 
     show_gender_percentage(ndx, "male", "#male-student-percentage"); 
+    
+    show_average_math_score(ndx);
 
     dc.renderAll(); // Essential command for Chart to appear
 }
@@ -20,8 +26,18 @@ function show_student_selector(ndx) {
 
     dc.selectMenu('#student-selector') // x2 properties only
         .dimension(studentDim)
-        .group(studentGroup);
+        .group(studentGroup)
+        .title(function(d) {
+            return d.key;
+        });
         
+        show_student_selector.order = function (order) { // Student data now re-ordered to allow view from 1-1,000 in 'intro' dropdown box
+        if (!arguments.length) {
+            return order;
+        }
+        order = order;
+        return show_student_selector;
+    };
 }
 
 function show_gender_selector(ndx) {
@@ -31,6 +47,7 @@ function show_gender_selector(ndx) {
      dc.selectMenu('#gender-selector') // x2 properties only
         .dimension(genderDim)
         .group(genderGroup);
+        
 }
 
 function show_gender_percentage(ndx, gender, element) {
@@ -67,3 +84,49 @@ function show_gender_percentage(ndx, gender, element) {
         })
         .group(studentgenderPercentage);
 }
+
+function show_average_math_score(ndx, math_score, element) {
+    var dim = ndx.dimension(dc.pluck('math_score'));
+    
+    function add_item(p, v) { // p is an accumulator and keeps track of the total count
+        p.count++; // Increment the count in our p object  
+        p.total += v.math_score; // We increment our total by the value of the maths score we're looking at
+        p.average = p.total / p.count; // Average maths score calculation
+        return p; // Important command. Absence will create errors 
+    }
+    
+    function remove_item(p, v) { // v represents each of the data items being added or removed 
+        p.count--; // Reduce the count in our p object  
+            if(p.count == 0) { // Count value will not be less than Zero & avoid a DIV#0 error
+                p.total = 0;
+                p.average = 0;
+            }else {
+                p.total -= v.math_score; // We reduce our total by the value of the math score we're looking at
+                p.average = p.total / p.count; // Average score calculation
+            }
+        return p;
+    }
+    
+    function initialise() { // Creates an initial value for p
+        return {count: 0, total: 0, average: 0};
+    }
+    
+    var averageMathScore = dim.group().reduce(add_item, remove_item, initialise); 
+    
+    console.log(averageMathScore.all());  // Console test is good
+    
+    dc.numberDisplay("#average-math-score")
+        .formatNumber(d3.format('')) 
+        .valueAccessor(function(d) {
+            if (isNaN(d.math_score_count == "NaN")) {
+                return 0;
+            }
+            else {
+                return (d.math_score_count / d.total);
+            }
+        })
+        .group(averageMathScore);
+        
+    
+}
+    
