393038fee1a0ec3248ce8506007be3f71edcec4d
Paul Friel <paul.b.friel@me.com>
2019-04-24 16:30:46 +0000

    JS function for toggle of articles completed

---
+++ static/js/helper.js
@@ -0,0 +1,9 @@
+
+/* ------------------------------------------------ Hide Content -*/
+
+$('.hide-content-btn').click(function() {
+    $(this).parent().nextAll('.hide-content-toggle').slideToggle();
+    $(this).parent().parent().parent().toggleClass('eq-column-height');
+    $(this).toggleClass('fa-angle-down');
+    $(this).toggleClass('fa-angle-up');
+});
