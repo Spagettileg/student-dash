7af06e0701847a92c63dfd1a70b1cd8597e35043
Ubuntu <ubuntu@ip-172-31-46-228.eu-west-1.compute.internal>
2021-02-25 21:34:11 +0000

    New return to top button fucntion added. Button will only appear once user has scrolled down page by minimum of 575px.

--- static/js/helper.js
+++ static/js/helper.js
@@ -22,3 +22,21 @@ $(document).ready(function () {
     $('#developer-years').text('2018 - ' + new Date().getFullYear());
     
 });
+
+// Scroll to top of dashboard function
+
+    $("a[href='#move-up']").click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 800);
+        return false;
+    });
+
+    // If position of vertical scroll is above 575px, for 
+    // return to top button to disappear.
+    
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 575) {
+            $('.move-top').fadeIn();
+        } else {
+            $('.move-top').fadeOut();
+        }
+    });
